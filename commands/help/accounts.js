5a6f1be0cd5ca28e6503379cd595fcb01f6c3d45
emmaodia <odia.emma@gmail.com>
2021-06-28 02:29:16 +0100

    chore: update accounts to take account and return results

--- commands/help/accounts.js
+++ commands/help/accounts.js
@@ -5,20 +5,26 @@ module.exports = {
 	name: 'accounts',
 	description: 'Get ALL accounts information!',
 	async execute(message, args) {
-		const file = await fetch('https://testnet-algorand.api.purestake.io/idx2/v2/accounts/AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAY5HFKQ/', {
+		const string = args.slice(0, 1).toString();
+
+		const file = await fetch(`https://testnet-algorand.api.purestake.io/idx2/v2/accounts/${string}`, {
 			method: 'GET',
 			headers: { 'Content-Type': 'application/json',
 				'x-api-key' : process.env.API_KEY,
 			},
 		}).then(response => response.json());
 		console.log(file);
-
+		console.log(args);
+		console.log(string);
 		const embed = new MessageEmbed()
 			.addFields({ name: 'Account Address', value: file.account.address },
 				{ name: 'Account Amount', value: file.account.amount },
 				{ name: 'Account Rewards', value: file.account.rewards },
 				{ name: 'Account Round', value: file.account.round },
-				{ name: 'Account Status', value: file.account.status });
+				{ name: 'Account Status', value: file.account.status },
+				// View more on the explorer
+				// { name: 'View More', value: file.account.status }
+			);
 		message.channel.send(embed);
 	},
 };
